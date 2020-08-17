9e325777baafab065f1b4d1a22f2f2f392c665d0
xiongyongjiang <xiongyongjiangyh@163.com>
2020-08-17 16:47:08 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'prettier/unicorn',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/extensions': [
+      ERROR,
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+        js: 'never',
+      },
+    ],
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Hello from './hello';
+
+const b = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+
+const fn = (a: number) => {
+  console.log(a + 1);
+};
